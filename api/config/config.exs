132af110e46fa10874f81141f11b6e9beb308dda
Andy <andy@gettimesaved.com>
2021-01-12 22:31:11 -0500

    Initialize Phoenix web interface

--- api/config/config.exs
+++ api/config/config.exs
@@ -9,6 +9,17 @@
 # move said applications out of the umbrella.
 import Config
 
+config :web_interface,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :web_interface, WebInterface.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "xoiYghdOAjm3RT3CnxaB3KbpOw8pW9V4cL7TmppmQrWyZOs2NoF8GzRKbXQIBDmc",
+  render_errors: [view: WebInterface.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: WebInterface.PubSub,
+  live_view: [signing_salt: "WwAyNu3R"]
+
 # Sample configuration:
 #
 #     config :logger, :console,
@@ -16,3 +27,15 @@ import Config
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
 #
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
