c842ade6286f2814f2e7b9a3feb9f3ceb3461f08
Andy <andy@gettimesaved.com>
2021-01-25 01:08:22 -0500

    Rename header

--- client/pages/stats.tsx
+++ client/pages/stats.tsx
@@ -17,7 +17,7 @@ export default function Stats() {
 
       <header className="py-10 bg-gray-800 pb-28">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-white">Dashboard</h1>
+          <h1 className="text-3xl font-bold text-white">Rushing statistics</h1>
         </div>
       </header>
 
