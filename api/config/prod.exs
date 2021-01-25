b7a06cc66cd891bbb8b808a7ee01d17502c837b7
Andy <andy@gettimesaved.com>
2021-01-25 03:57:40 -0500

    Add config for gigalixir

--- api/config/prod.exs
+++ api/config/prod.exs
@@ -46,3 +46,17 @@ config :rushing_web, RushingWeb.Endpoint,
 #       force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
+
+config :rushing, RushingWeb.Endpoint,
+  # Possibly not needed, but doesn't hurt
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
+
+config :rushing, Database.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+  pool_size: 2
