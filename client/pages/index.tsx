2e6b08388a28c590d622db0f133a293eea9fba75
Andy <andy@gettimesaved.com>
2021-01-25 00:38:31 -0500

    Add navbar and cut out a bunch of index

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import { Navbar } from "../components/Navbar";
 
 export default function Home() {
   return (
@@ -35,6 +36,7 @@ require('@tailwindcss/forms'),
   ```
 */}
       <div className="bg-white">
+        <Navbar />
         <main>
           {/* Hero section */}
           <div className="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-48">
@@ -48,63 +50,15 @@ require('@tailwindcss/forms'),
                   />
                 </div>
                 <div className="mt-20">
-                  <div>
-                    <a href="#" className="inline-flex space-x-4">
-                      <span className="rounded bg-rose-50 px-2.5 py-1 text-xs font-semibold text-rose-500 tracking-wide uppercase">
-                        What's new
-                      </span>
-                      <span className="inline-flex items-center text-sm font-medium text-rose-500 space-x-1">
-                        <span>Just shipped version 0.1.0</span>
-                        {/* Heroicon name: chevron-right */}
-                        <svg
-                          className="h-5 w-5"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                          aria-hidden="true"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </span>
-                    </a>
-                  </div>
                   <div className="mt-6 sm:max-w-xl">
                     <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
-                      Issue management for growing teams
+                      Only the cold, hard facts.
                     </h1>
                     <p className="mt-6 text-xl text-gray-500">
-                      Anim aute id magna aliqua ad ad non deserunt sunt. Qui
-                      irure qui lorem cupidatat commodo.
+                      We'll help you find every rushing stat you'll ever need.
                     </p>
                   </div>
-                  <form
-                    action="#"
-                    className="mt-12 sm:max-w-lg sm:w-full sm:flex"
-                  >
-                    <div className="min-w-0 flex-1">
-                      <label htmlFor="hero_email" className="sr-only">
-                        Email address
-                      </label>
-                      <input
-                        id="hero_email"
-                        type="email"
-                        className="block w-full border border-gray-300 rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-rose-500 focus:ring-rose-500"
-                        placeholder="Enter your email"
-                      />
-                    </div>
-                    <div className="mt-4 sm:mt-0 sm:ml-3">
-                      <button
-                        type="submit"
-                        className="block w-full rounded-md border border-transparent px-5 py-3 bg-rose-500 text-base font-medium text-white shadow hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 sm:px-10"
-                      >
-                        Notify me
-                      </button>
-                    </div>
-                  </form>
+
                   <div className="mt-6">
                     <div className="inline-flex items-center divide-x divide-gray-300">
                       <div className="flex-shrink-0 flex pr-5">
@@ -213,250 +167,16 @@ require('@tailwindcss/forms'),
                 <div className="relative pl-4 -mr-40 sm:mx-auto sm:max-w-3xl sm:px-0 lg:max-w-none lg:h-full lg:pl-12">
                   <Image
                     className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
-                    src="/player.png"
-                    width={642}
-                    height={388}
-                    layout="intrinsic"
+                    src="/player-red.png"
+                    width={587}
+                    height={425}
+                    layout="responsive"
                     alt=""
                   />
                 </div>
               </div>
             </div>
           </div>
-          {/* Testimonial/stats section */}
-          <div className="relative mt-20">
-            <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start">
-              <div className="relative sm:py-16 lg:py-0">
-                <div
-                  aria-hidden="true"
-                  className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen"
-                >
-                  <div className="absolute inset-y-0 right-1/2 w-full bg-gray-50 rounded-r-3xl lg:right-72" />
-                  <svg
-                    className="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12"
-                    width={404}
-                    height={392}
-                    fill="none"
-                    viewBox="0 0 404 392"
-                  >
-                    <defs>
-                      <pattern
-                        id="02f20b47-fd69-4224-a62a-4c9de5c763f7"
-                        x={0}
-                        y={0}
-                        width={20}
-                        height={20}
-                        patternUnits="userSpaceOnUse"
-                      >
-                        <rect
-                          x={0}
-                          y={0}
-                          width={4}
-                          height={4}
-                          className="text-gray-200"
-                          fill="currentColor"
-                        />
-                      </pattern>
-                    </defs>
-                    <rect
-                      width={404}
-                      height={392}
-                      fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)"
-                    />
-                  </svg>
-                </div>
-                <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
-                  {/* Testimonial card*/}
-                  <div className="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
-                    <img
-                      className="absolute inset-0 h-full w-full object-cover"
-                      src="https://images.unsplash.com/photo-1521510895919-46920266ddb3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&fp-x=0.5&fp-y=0.6&fp-z=3&width=1440&height=1440&sat=-100"
-                      alt=""
-                    />
-                    <div
-                      className="absolute inset-0 bg-rose-500"
-                      style={{ mixBlendMode: "multiply" }}
-                    />
-                    <div className="absolute inset-0 bg-gradient-to-t from-rose-600 via-rose-600 opacity-90" />
-                    <div className="relative px-8">
-                      <div>
-                        <img
-                          className="h-12"
-                          src="https://tailwindui.com/img/logos/workcation.svg?color=white"
-                          alt="Workcation"
-                        />
-                      </div>
-                      <blockquote className="mt-8">
-                        <div className="relative text-lg font-medium text-white md:flex-grow">
-                          <svg
-                            className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-rose-400"
-                            fill="currentColor"
-                            viewBox="0 0 32 32"
-                            aria-hidden="true"
-                          >
-                            <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-                          </svg>
-                          <p className="relative">
-                            Tincidunt integer commodo, cursus etiam aliquam
-                            neque, et. Consectetur pretium in volutpat, diam.
-                            Montes, magna cursus nulla feugiat dignissim id
-                            lobortis amet.
-                          </p>
-                        </div>
-                        <footer className="mt-4">
-                          <p className="text-base font-semibold text-rose-200">
-                            Sarah Williams, CEO at Workcation
-                          </p>
-                        </footer>
-                      </blockquote>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
-                {/* Content area */}
-                <div className="pt-12 sm:pt-16 lg:pt-20">
-                  <h2 className="text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl">
-                    On a mission to empower teams
-                  </h2>
-                  <div className="mt-6 text-gray-500 space-y-6">
-                    <p className="text-lg">
-                      Sagittis scelerisque nulla cursus in enim consectetur
-                      quam. Dictum urna sed consectetur neque tristique
-                      pellentesque. Blandit amet, sed aenean erat arcu morbi.
-                      Cursus faucibus nunc nisl netus morbi vel porttitor vitae
-                      ut. Amet vitae fames senectus vitae.
-                    </p>
-                    <p className="text-base leading-7">
-                      Sollicitudin tristique eros erat odio sed vitae, consequat
-                      turpis elementum. Lorem nibh vel, eget pretium arcu vitae.
-                      Eros eu viverra donec ut volutpat donec laoreet quam urna.
-                      Sollicitudin tristique eros erat odio sed vitae, consequat
-                      turpis elementum. Lorem nibh vel, eget pretium arcu vitae.
-                      Eros eu viverra donec ut volutpat donec laoreet quam urna.
-                    </p>
-                    <p className="text-base leading-7">
-                      Rhoncus nisl, libero egestas diam fermentum dui. At quis
-                      tincidunt vel ultricies. Vulputate aliquet velit faucibus
-                      semper. Pellentesque in venenatis vestibulum consectetur
-                      nibh id. In id ut tempus egestas. Enim sit aliquam nec, a.
-                      Morbi enim fermentum lacus in. Viverra.
-                    </p>
-                  </div>
-                </div>
-                {/* Stats section */}
-                <div className="mt-10">
-                  <dl className="grid grid-cols-2 gap-x-4 gap-y-8">
-                    <div className="border-t-2 border-gray-100 pt-6">
-                      <dt className="text-base font-medium text-gray-500">
-                        Founded
-                      </dt>
-                      <dd className="text-3xl font-extrabold tracking-tight text-gray-900">
-                        2021
-                      </dd>
-                    </div>
-                    <div className="border-t-2 border-gray-100 pt-6">
-                      <dt className="text-base font-medium text-gray-500">
-                        Employees
-                      </dt>
-                      <dd className="text-3xl font-extrabold tracking-tight text-gray-900">
-                        5
-                      </dd>
-                    </div>
-                    <div className="border-t-2 border-gray-100 pt-6">
-                      <dt className="text-base font-medium text-gray-500">
-                        Beta Users
-                      </dt>
-                      <dd className="text-3xl font-extrabold tracking-tight text-gray-900">
-                        521
-                      </dd>
-                    </div>
-                    <div className="border-t-2 border-gray-100 pt-6">
-                      <dt className="text-base font-medium text-gray-500">
-                        Raised
-                      </dt>
-                      <dd className="text-3xl font-extrabold tracking-tight text-gray-900">
-                        $25M
-                      </dd>
-                    </div>
-                  </dl>
-                  <div className="mt-10">
-                    <a href="#" className="text-base font-medium text-rose-500">
-                      Learn more about how we're changing the world&nbsp;→
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* Logo cloud section */}
-          <div className="mt-32">
-            <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
-              <div className="lg:grid lg:grid-cols-2 lg:gap-24 lg:items-center">
-                <div>
-                  <h2 className="text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
-                    Backed by world-renowned investors
-                  </h2>
-                  <p className="mt-6 max-w-3xl text-lg leading-7 text-gray-500">
-                    Sagittis scelerisque nulla cursus in enim consectetur quam.
-                    Dictum urna sed consectetur neque tristique pellentesque.
-                    Blandit amet, sed aenean erat arcu morbi. Cursus faucibus
-                    nunc nisl netus morbi vel porttitor vitae ut. Amet vitae
-                    fames senectus vitae.
-                  </p>
-                  <div className="mt-6">
-                    <a href="#" className="text-base font-medium text-rose-500">
-                      Meet our investors and advisors&nbsp;→
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-12 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
-                  <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                    <img
-                      className="max-h-12"
-                      src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
-                      alt="Workcation"
-                    />
-                  </div>
-                  <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                    <img
-                      className="max-h-12"
-                      src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
-                      alt="Mirage"
-                    />
-                  </div>
-                  <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                    <img
-                      className="max-h-12"
-                      src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
-                      alt="Tuple"
-                    />
-                  </div>
-                  <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                    <img
-                      className="max-h-12"
-                      src="https://tailwindui.com/img/logos/laravel-logo-gray-400.svg"
-                      alt="Laravel"
-                    />
-                  </div>
-                  <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                    <img
-                      className="max-h-12"
-                      src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
-                      alt="StaticKit"
-                    />
-                  </div>
-                  <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
-                    <img
-                      className="max-h-12"
-                      src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
-                      alt="Statamic"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>{" "}
         </main>
         {/* Footer section */}
         <footer className="mt-24 bg-gray-900 sm:mt-12">
@@ -588,7 +308,7 @@ require('@tailwindcss/forms'),
               </a>
             </div>
             <p className="mt-8 text-center text-base text-gray-400">
-              © 2020 Workflow, Inc. All rights reserved.
+              © 2020 Rushing, Inc. All rights reserved.
             </p>
           </div>
         </footer>
