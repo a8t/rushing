19467bb88a4aba5fb3de7bd9f5a17475cdfa6cde
Andy <andy@gettimesaved.com>
2021-01-25 01:49:29 -0500

    Add basic table

--- client/pages/stats.tsx
+++ client/pages/stats.tsx
@@ -1,8 +1,72 @@
+import React from "react";
 import Head from "next/head";
 import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
+import { useTable } from "react-table";
 
-export default function Stats() {
+export async function getServerSideProps({ query }) {
+  const url = process.env.NEXT_PUBLIC_API_URL + "/api/rushing_statistics";
+  const res = await fetch(url);
+
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { stats: data.data }, // will be passed to the page component as props
+  };
+}
+
+export default function Stats({ stats }) {
+  const memoizedData = React.useMemo(() => stats, [stats]);
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Player",
+        accessor: "player_name",
+        Cell: ({ row: { original } }) => {
+          return (
+            <div>
+              <div className="text-sm font-medium text-gray-900">
+                {original.player_name}
+              </div>
+              <div className="text-sm text-gray-500">
+                {original.player_position} • {original.player_team}
+              </div>
+            </div>
+          );
+        },
+      },
+      { Header: "Att/G", accessor: "attempts" },
+      { Header: "Att", accessor: "attempts_per_game_average" },
+      { Header: "Yds", accessor: "total_yards" },
+      { Header: "Avg", accessor: "yards_per_attempt_average" },
+      { Header: "Yds/G", accessor: "yards_per_game" },
+      { Header: "TD", accessor: "total_rushing_touchdowns" },
+      { Header: "Lng", accessor: "longest_rush" },
+      { Header: "1st", accessor: "first_downs" },
+      { Header: "1st%", accessor: "first_down_percentage" },
+      { Header: "20+", accessor: "over_twenty_yards" },
+      { Header: "40+", accessor: "over_forty_yards" },
+      { Header: "FUM", accessor: "fumbles" },
+    ],
+
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data: memoizedData });
+
   return (
     <div
       style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
@@ -27,86 +91,73 @@ export default function Stats() {
             <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                  <table className="min-w-full divide-y divide-gray-200">
+                  <table
+                    className="min-w-full divide-y divide-gray-200"
+                    {...getTableProps()}
+                  >
                     <thead className="bg-gray-50">
-                      <tr>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >
-                          Name
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >
-                          Title
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >
-                          Email
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >
-                          Role
-                        </th>
-                        <th scope="col" className="relative px-6 py-3">
-                          <span className="sr-only">Edit</span>
-                        </th>
-                      </tr>
+                      {headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                          {
+                            // Loop over the headers in each row
+
+                            headerGroup.headers.map((column) => (
+                              // Apply the header cell props
+
+                              <th
+                                {...column.getHeaderProps()}
+                                scope="col"
+                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                              >
+                                {column.render("Header")}
+                              </th>
+                            ))
+                          }
+                        </tr>
+                      ))}
                     </thead>
-                    <tbody>
-                      {/* Odd row */}
-                      <tr className="bg-white">
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          Jane Cooper
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          Regional Paradigm Technician
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          jane.cooper@example.com
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          Admin
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                          <a
-                            href="#"
-                            className="text-indigo-600 hover:text-indigo-900"
-                          >
-                            Edit
-                          </a>
-                        </td>
-                      </tr>
-                      {/* Even row */}
-                      <tr className="bg-gray-50">
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          Cody Fisher
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          Product Directives Officer
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          cody.fisher@example.com
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          Owner
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                          <a
-                            href="#"
-                            className="text-indigo-600 hover:text-indigo-900"
-                          >
-                            Edit
-                          </a>
-                        </td>
-                      </tr>
-                      {/* More items... */}
+                    <tbody {...getTableBodyProps()}>
+                      {
+                        // Loop over the table rows
+
+                        rows.map((row, index) => {
+                          // Prepare the row for display
+
+                          prepareRow(row);
+
+                          const isDarkRow = index % 2 === 0;
+
+                          return (
+                            // Apply the row props
+
+                            <tr
+                              {...row.getRowProps()}
+                              className={isDarkRow ? "bg-gray-50" : "bg-white"}
+                            >
+                              {
+                                // Loop over the rows cells
+
+                                row.cells.map((cell) => {
+                                  // Apply the cell props
+
+                                  return (
+                                    <td
+                                      {...cell.getCellProps()}
+                                      className="px-6 py-4 whitespace-nowrap text-sm  text-gray-900"
+                                    >
+                                      {
+                                        // Render the cell contents
+
+                                        cell.render("Cell")
+                                      }
+                                    </td>
+                                  );
+                                })
+                              }
+                            </tr>
+                          );
+                        })
+                      }
                     </tbody>
                   </table>
                 </div>
